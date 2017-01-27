f4d28e7fe9cdd20f1fc882127d066f95f7266b07
Jufebrown <jufebrown@gmail.com>
2017-01-27 14:32:26 -0600

    configured firebase

--- app/app.js
+++ app/app.js
@@ -2,29 +2,29 @@ const app = angular.module('musHist10', ['ngRoute'])
 
 app.config(($routeProvider) => {
 
-    // Initialize Firebase
-    const config = {
-      apiKey: "AIzaSyA83A4Nz9ZO32NiPeNF8zdcYPTTPb3PYCQ",
-      authDomain: "c17-firebase-auth-jufe.firebaseapp.com",
-      databaseURL: "https://c17-firebase-auth-jufe.firebaseio.com",
-      storageBucket: "c17-firebase-auth-jufe.appspot.com",
-      messagingSenderId: "653707776406"
-    };
-    firebase.initializeApp(config);
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDZ268yBuiZmo5SMAVoLQVCdvLYGS8SpzM",
+    authDomain: "music-history-10.firebaseapp.com",
+    databaseURL: "https://music-history-10.firebaseio.com",
+    storageBucket: "music-history-10.appspot.com",
+    messagingSenderId: "403166165501"
+  };
+  firebase.initializeApp(config);
 
-    $routeProvider
-      .when('/', {
-        controller: 'SongCtrl',
-        templateUrl: 'partials/song-list.html'
-      })
-      .when('/detail, {
-        controller: 'DetailCtrl',
-        templateUrl: '/partials/song-detail.html',
-        resolve: {
-          weather: (detailFactory, $route) => {songFactory.getDetail($route.current.params.song)},
-          user (authFactory, $location) {
-            authFactory.getUser().catch(() => $location.url('/'))
-          },
-        }
-      })
+  $routeProvider
+  .when('/', {
+    controller: 'SongCtrl',
+    templateUrl: 'partials/song-list.html'
   })
+  .when('/detail, {
+    controller: 'DetailCtrl',
+    templateUrl: '/partials/song-detail.html',
+    resolve: {
+      weather: (detailFactory, $route) => {songFactory.getDetail($route.current.params.song)},
+      user (authFactory, $location) {
+        authFactory.getUser().catch(() => $location.url('/'))
+      },
+    }
+  })
+})
